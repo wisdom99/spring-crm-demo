b68f5165d8ac513e625a353bd535515c52259606
Wisdom Ifeanyi <ifeanyiwisdom388@gmail.com>
2021-07-25 21:57:48 +0100

    Initial commit

---
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -0,0 +1,79 @@
+package com.luv2code.springdemo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+	
+	//autowire the CustomerService
+	@Autowired
+	private CustomerService customerService;
+	
+	//add mapping for /GET customers
+	@GetMapping("/customers")
+	public List<Customer> getCustomers(){
+		
+		return customerService.getCustomers();
+	}
+	
+	//add mapping for GET /customers/{customerId}
+	@GetMapping ("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		
+		Customer theCustomer = customerService.getCustomer(customerId);
+		
+		if (theCustomer==null )
+			throw new CustomerNotFoundException("Customer id not found - "+ customerId);
+		return  theCustomer;
+	}
+	
+	//add mappping for POST/customers -add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		//set id to 0 just force a save of new item...instead of update
+		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	//add mapping for PUT /customers - update  existing customer 
+	@PutMapping("/customers")
+	public Customer updateCustomer (@RequestBody Customer theCustomer) {
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	//add mapping for DELETE /customers/{customerId} - delete customer
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		customerService.deleteCustomer(customerId);
+		
+		Customer tempCustomer = customerService.getCustomer(customerId);
+		
+		//throw exception if null
+		if (tempCustomer==null) {
+			throw new CustomerNotFoundException("Customer id not found - "+ customerId);
+		}
+		return " Deleted customer id - " + customerId ;
+	}
+	
+}
+	
